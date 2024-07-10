da9e3963ab4255430aee5a19bd818898c6bdf084
KatalinLengyel <158131186+KatalinLengyel@users.noreply.github.com>
2024-07-10 09:55:31 +0300

    Tema9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector(".form");
+
+formEl.addEventListener("submit", onSubmitForm);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({position, delay});
+        } else {
+          reject({position, delay});
+        }
+    }, delay);
+  });
+};
+
+function onSubmitForm(entry) {
+  entry.preventDefault();
+  let delay = Number(formEl.delay.value);
+
+  for (let i = 1; i <= formEl.amount.value; i++) {
+    createPromise(i, delay)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  delay += Number(formEl.step.value);  
+  };
+};
